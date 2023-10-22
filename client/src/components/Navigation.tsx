5daad717945c8825e2d4869b29ea8482ddfbcf32
warren Colliver <colliverwarren@gmail.com>
2023-10-22 18:48:40 +0100

    Fix wallet connection

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,10 +1,13 @@
 import React, { useCallback, useState } from 'react';
 import Onboard, { WalletState } from '@web3-onboard/core'
+import injectedModule from '@web3-onboard/injected-wallets'
 
 import SendTransaction from './SendTransaction';
 
+const injected = injectedModule()
+
 const onboard = Onboard({
-  wallets: [],
+  wallets: [injected],
   chains: [
     {
       id: '123456',
